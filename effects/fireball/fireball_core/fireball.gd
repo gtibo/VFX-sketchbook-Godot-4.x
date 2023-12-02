ea54d6ea5c76dd7973b6a5dddf7b014a246270e0
Tibo <goiffonthibaud@protonmail.com>
2023-12-02 16:32:05 +0100

    fix: transform

--- effects/fireball/fireball_core/fireball.gd
+++ effects/fireball/fireball_core/fireball.gd
@@ -6,6 +6,19 @@ extends Node3D
 
 signal is_out
 
+var angular_vel : Vector3 = Vector3(0.0, 0.0, 0.0)
+var last_direction : Vector3
+
+func _process(delta):
+	var direction = global_transform.basis.y
+	angular_vel = angular_vel.move_toward((direction - last_direction), 3.0 * delta)
+	last_direction = direction
+	angular_vel = angular_vel.move_toward(Vector3.ZERO, 0.1 * delta)
+	
+	fireball_shell_mesh.material_override.set_shader_parameter("def_x", -angular_vel.x)
+	fireball_shell_mesh.material_override.set_shader_parameter("def_z", angular_vel.z)
+	
+	
 func out():
 	fireball_shell_mesh.hide()
 	fire_trail.emitting = false
--- effects/fireball/scripts/simple_projectile.gd
+++ effects/fireball/scripts/simple_projectile.gd
@@ -9,7 +9,7 @@ extends Node3D
 
 var target_node : Node3D
 var velocity : Vector3
-var speed = 0.4
+var speed = 1.0
 
 func launch(start_position : Vector3, start_velocity : Vector3, target : Node3D):
 	global_position = start_position
@@ -25,7 +25,7 @@ func launch(start_position : Vector3, start_velocity : Vector3, target : Node3D)
 func _ready():
 	hit_zone_3d.connect("hit", func(hit_pos, hit_normal):
 		fireball.out()
-		set_process(false)
+		set_physics_process(false)
 		var impact = impact_scene.instantiate()
 		add_sibling(impact)
 		impact.global_position = hit_pos
@@ -33,10 +33,10 @@ func _ready():
 		)
 	fireball.connect("is_out", queue_free)
 
-func _process(delta):
+func _physics_process(delta):
 	if target_node == null: return
-	var direction = (target_node.global_position - global_position).limit_length(speed)
-	velocity = velocity.move_toward(direction, speed * delta * 3.0)
+	var direction = (target_node.global_position - global_position).normalized()
+	velocity = velocity.move_toward(direction * speed, speed * 4.0 * delta)
 	position += velocity
 	_look_at_velocity()
 	
